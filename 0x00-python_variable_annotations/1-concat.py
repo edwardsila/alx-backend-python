7b4b01d0e37cbd4497baf0547474659b33a4ed65
edwardsila <edwardsila000@gmail.com>
2024-07-04 20:19:22 +0000

    concatenate

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+annotated function that concatenate two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ returns concatenated string """
+    return str1 + str2
