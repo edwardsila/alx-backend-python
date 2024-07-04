927497ff9f99032c3902c05779336270b2015af0
edwardsila <edwardsila000@gmail.com>
2024-07-04 20:13:04 +0000

    updated add fn

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python3
-"""
-type-annotated function
-"""
+#!/usr/bin/env python3
+
+'''
+    Basic annotations for variables.
+'''
 
 
 def add(a: float, b: float) -> float:
-    """
-       Return sum of two floats
-    """
+    '''
+        Returns sum of two floats.
+    '''
     return a + b
