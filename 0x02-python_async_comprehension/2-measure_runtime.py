437a605ef774780b8d8adc678732ab22d5033b45
edwardsila <edwardsila000@gmail.com>
2024-07-12 09:42:51 +0000

    measure runtime

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+runtime for four paralle comprehesion
+"""
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ measure the runtime """
+    start_time = time.time()
+    await asyncio.gather(
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension()
+    )
+    stop_time = time.time()
+    return stop_time - start_time
