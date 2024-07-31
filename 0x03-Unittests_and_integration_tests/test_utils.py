181d95b67e6e78f520443a7eb7d7f65a40bade4e
edwardsila <edwardsila000@gmail.com>
2024-07-31 10:27:16 +0000

    mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -40,12 +40,9 @@ class TestGetjson(TestCase):
         ("http://holberton.io" {"payload": False})
     ])
     @patch('requests.get')
-    def test_get_json(self, test_url, test_payload, mock_get):
-        mock_response = Mock()
-        mock_response.json.return_value = test_payload
-        mock_get.return_value = mock_response
-
+    def test_get_json(self, test_url, test_payload, mock_requests_get):
+        """ tests the get_json method """
+        mock_requests_get.return_value.json.return_value = test_payload
         result = get_json(test_url)
         self.assertEqual(result, test_payload)
-
-        mock_get.assert_called_once_with(test_url)
+        mock_requests_get.assert_called_once_with(test_url)
